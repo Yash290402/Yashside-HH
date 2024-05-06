7ded076460851b4dd486147d019bf8cf74f94651
Yash290402 <yash56774@gmail.com>
2024-05-06 21:03:55 +0530

    delete unnessary code

--- frontend/book/book.js
+++ frontend/book/book.js
@@ -1,38 +1,4 @@
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const timeSlots = document.querySelectorAll('.time-slot');
-
-//     // Add event listeners to each time slot button
-//     timeSlots.forEach(slot => {
-//         slot.addEventListener('click', function () {
-//             const selectedTime = this.getAttribute('data-time');
-//             const ischecked = this.getAttribute('checked');
-//             bookTimeSlot(selectedTime, ischecked);
-//         });
-//     });
-
-//     // Function to book a time slot
-//     async function bookTimeSlot(date, ischecked) {
-//         try {
-//             const response = await fetch('http://localhost:5500/api/v1/users/book', {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//                 },
-//                 body: JSON.stringify({ date, ischecked })
-//             });
-
-//             if (!response.ok) {
-//                 throw new Error('Failed to book time slot');
-//             }
-
-//             const data = await response.json();
-//             console.log(data.message); // Log the response message from the server
-//         } catch (error) {
-//             console.error('An error occurred:', error);
-//         }
-//     }
-// });
 
 
 document.addEventListener("DOMContentLoaded", function () {
